39f7818fb7c93d0b79ef663bc30f980e38efe89b
Marcos Piccinini <x@nofxx.com>
2009-06-19 08:25:21 -0300

    and using ENV on env... heh

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,19 @@
 require 'spec'
-#require 'cucumber/rails/rspec'
-require 'webrat/core/matchers'
+
+if ENV['CUC_ENV'] == 'webrat'
+  require 'webrat/core/matchers'
+  require File.expand_path(File.dirname(__FILE__) + '/../../lib/webratcuke')
+else
+  require File.expand_path(File.dirname(__FILE__) + '/../../lib/watircuke')
+
+  Before do
+    @environment = "http://github.com/"
+    #  sleep 1
+  end
+
+  # "after all"
+  at_exit do
+    @browser.close if @browser
+  end
+
+end
